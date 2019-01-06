2dae506d2a3e6f6f0619e1e92c090af7e5471199
Xingchen Fu <40333215+JalenYes@users.noreply.github.com>
2019-01-06 12:44:28 -0500

    Add files via upload

---
+++ Quora/Kernel 1.py
@@ -0,0 +1,399 @@
+# 0. import statement
+import os
+import time
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import math
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Dense, Input, CuDNNLSTM, Embedding, Dropout, Activation, CuDNNGRU, Conv1D
+from keras.layers import Bidirectional, GlobalMaxPool1D, GlobalMaxPooling1D, GlobalAveragePooling1D
+from keras.layers import Input, Embedding, Dense, Conv2D, MaxPool2D, concatenate
+from keras.layers import Reshape, Flatten, Concatenate, Dropout, SpatialDropout1D
+from keras.optimizers import Adam
+from keras.models import Model
+from keras import backend as K
+from keras.engine.topology import Layer
+from keras import initializers, regularizers, constraints, optimizers, layers
+
+# 1. Preprocession
+
+## some config values
+embed_size = 300  # how big is each word vector
+max_features = 95000  # how many unique words to use (i.e num rows in embedding vector)
+maxlen = 70  # max number of words in a question to use
+
+## Puncts
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*',
+          '+', '\\', '•', '~', '@', '£',
+          '·', '_', '{', '}', '©', '^', '®', '`', '<', '→', '°', '€', '™', '›', '♥', '←', '×', '§', '″', '′', 'Â', '█',
+          '½', 'à', '…',
+          '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥',
+          '▓', '—', '‹', '─',
+          '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾',
+          'Ã', '⋅', '‘', '∞',
+          '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹',
+          '≤', '‡', '√']
+
+
+def clean_text(x):
+    x = str(x)
+    for punct in puncts:
+        x = x.replace(punct, f' {punct} ')
+    return x
+
+
+def split_text(x):
+    x = wordninja.split(x)
+    return '-'.join(x)
+
+
+def load_and_prec():
+    train_df = pd.read_csv("../input/train.csv")
+    test_df = pd.read_csv("../input/test.csv")
+
+    train_df["question_text"] = train_df["question_text"].str.lower()
+    test_df["question_text"] = test_df["question_text"].str.lower()
+
+    train_df["question_text"] = train_df["question_text"].apply(lambda x:clean_text(x))
+    test_df["question_text"] = test_df["question_text"].apply(lambda x:clean_text(x))
+
+    print("Train shape : ", train_df.shape)
+    print("Test shape : ", test_df.shape)
+
+    ## split data to train and validation
+    train_df, val_df = train_test_split(train_df, test_size=0.0001, random_state=0)
+
+    ## fill up the missing values
+    train_X = train_df["question_text"].fillna("_##_").values
+    val_X = val_df["question_text"].fillna("_##_").values
+    test_X = test_df["question_text"].fillna("_##_").values
+
+    ## Tokenize the sentences
+    tokenizer = Tokenizer(num_words=max_features)
+    tokenizer.fit_on_texts(list(train_X))
+    train_X = tokenizer.texts_to_sequences(train_X)
+    val_X = tokenizer.texts_to_sequences(val_X)
+    test_X = tokenizer.texts_to_sequences(test_X)
+
+    ## Pad the sentences
+    train_X = pad_sequences(train_X, maxlen=maxlen)
+    val_X = pad_sequences(val_X, maxlen=maxlen)
+    test_X = pad_sequences(test_X, maxlen=maxlen)
+
+    ## Get the target values
+    train_y = train_df['target'].values
+    val_y = val_df['target'].values
+
+    # shuffling the data
+    np.random.seed(2018)
+    trn_idx = np.random.permutation(len(train_X))
+    val_idx = np.random.permutation(len(val_X))
+
+    train_X = train_X[trn_idx]
+    val_X = val_X[val_idx]
+    train_y = train_y[trn_idx]
+    val_y = val_y[val_idx]
+
+    return train_X, val_X, test_X, train_y, val_y, tokenizer.word_index
+
+
+# 2. Load embeddings
+
+def load_glove(word_index):
+    EMBEDDING_FILE = '../input/embeddings/glove.840B.300d/glove.840B.300d.txt'
+
+    def get_coefs(word, *arr):
+        return word, np.asarray(arr, dtype='float32')
+
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE))
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean, emb_std = -0.005838499, 0.48782197
+    embed_size = all_embs.shape[1]
+
+    ## word_index = tokenizer.word_index
+    nb_words = min(max_features, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= max_features: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix
+
+
+def load_fasttext(word_index):
+    EMBEDDING_FILE = '../input/embeddings/wiki-news-300d-1M/wiki-news-300d-1M.vec'
+
+    def get_coefs(word, *arr):
+        return word, np.asarray(arr, dtype='float32')
+
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE) if len(o) > 100)
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean, emb_std = all_embs.mean(), all_embs.std()
+    embed_size = all_embs.shape[1]
+
+    # word_index = tokenizer.word_index
+    nb_words = min(max_features, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= max_features: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+    return embedding_matrix
+
+
+def load_para(word_index):
+    EMBEDDING_FILE = '../input/embeddings/paragram_300_sl999/paragram_300_sl999.txt'
+
+    def get_coefs(word, *arr):
+        return word, np.asarray(arr, dtype='float32')
+
+    embeddings_index = dict(
+        get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE, encoding="utf8", errors='ignore') if len(o) > 100)
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean, emb_std = -0.0053247833, 0.49346462
+    embed_size = all_embs.shape[1]
+    print(emb_mean, emb_std, "para")
+
+    # word_index = tokenizer.word_index
+    nb_words = min(max_features, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= max_features: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix
+
+
+# 3. Modeling
+
+## CNN Model
+### https://www.kaggle.com/yekenot/2dcnn-textclassifier
+
+def model_cnn(embedding_matrix):
+    filter_sizes = [1, 2, 3, 5]
+    num_filters = 36
+
+    inp = Input(shape=(maxlen,))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+    x = Reshape((maxlen, embed_size, 1))(x)
+
+    maxpool_pool = []
+    for i in range(len(filter_sizes)):
+        conv = Conv2D(num_filters, kernel_size=(filter_sizes[i], embed_size),
+                      kernel_initializer='he_normal', activation='elu')(x)
+        maxpool_pool.append(MaxPool2D(pool_size=(maxlen - filter_sizes[i] + 1, 1))(conv))
+
+    z = Concatenate(axis=1)(maxpool_pool)
+    z = Flatten()(z)
+    z = Dropout(0.1)(z)
+
+    outp = Dense(1, activation="sigmoid")(z)
+
+    model = Model(inputs=inp, outputs=outp)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
+
+
+## Attention layer
+### https://www.kaggle.com/suicaokhoailang/lstm-attention-baseline-0-652-lb
+
+class Attention(Layer):
+    def __init__(self, step_dim,
+                 W_regularizer=None, b_regularizer=None,
+                 W_constraint=None, b_constraint=None,
+                 bias=True, **kwargs):
+        self.supports_masking = True
+        self.init = initializers.get('glorot_uniform')
+
+        self.W_regularizer = regularizers.get(W_regularizer)
+        self.b_regularizer = regularizers.get(b_regularizer)
+
+        self.W_constraint = constraints.get(W_constraint)
+        self.b_constraint = constraints.get(b_constraint)
+
+        self.bias = bias
+        self.step_dim = step_dim
+        self.features_dim = 0
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight((input_shape[-1],),
+                                 initializer=self.init,
+                                 name='{}_W'.format(self.name),
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
+        self.features_dim = input_shape[-1]
+
+        if self.bias:
+            self.b = self.add_weight((input_shape[1],),
+                                     initializer='zero',
+                                     name='{}_b'.format(self.name),
+                                     regularizer=self.b_regularizer,
+                                     constraint=self.b_constraint)
+        else:
+            self.b = None
+
+        self.built = True
+
+    def compute_mask(self, input, input_mask=None):
+        return None
+
+    def call(self, x, mask=None):
+        features_dim = self.features_dim
+        step_dim = self.step_dim
+
+        eij = K.reshape(K.dot(K.reshape(x, (-1, features_dim)),
+                              K.reshape(self.W, (features_dim, 1))), (-1, step_dim))
+
+        if self.bias:
+            eij += self.b
+
+        eij = K.tanh(eij)
+
+        a = K.exp(eij)
+
+        if mask is not None:
+            a *= K.cast(mask, K.floatx())
+
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+
+        a = K.expand_dims(a)
+        weighted_input = x * a
+        return K.sum(weighted_input, axis=1)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0], self.features_dim
+
+
+## LSTM models
+
+def model_lstm_atten(embedding_matrix):
+    inp = Input(shape=(maxlen,))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=False)(inp)
+    x = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x)
+    x = Bidirectional(CuDNNLSTM(64, return_sequences=True))(x)
+    x = Attention(maxlen)(x)
+    x = Dense(64, activation="relu")(x)
+    x = Dense(1, activation="sigmoid")(x)
+    model = Model(inputs=inp, outputs=x)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
+
+
+def model_gru_srk_atten(embedding_matrix):
+    inp = Input(shape=(maxlen,))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+    x = Bidirectional(CuDNNGRU(64, return_sequences=True))(x)
+    x = Attention(maxlen)(x)  # New
+    x = Dense(16, activation="relu")(x)
+    x = Dropout(0.1)(x)
+    x = Dense(1, activation="sigmoid")(x)
+    model = Model(inputs=inp, outputs=x)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
+
+
+def model_lstm_du(embedding_matrix):
+    inp = Input(shape=(maxlen,))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+    x = Bidirectional(CuDNNGRU(64, return_sequences=True))(x)
+    avg_pool = GlobalAveragePooling1D()(x)
+    max_pool = GlobalMaxPooling1D()(x)
+    conc = concatenate([avg_pool, max_pool])
+    conc = Dense(64, activation="relu")(conc)
+    conc = Dropout(0.1)(conc)
+    outp = Dense(1, activation="sigmoid")(conc)
+
+    model = Model(inputs=inp, outputs=outp)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+def model_gru_atten_3(embedding_matrix):
+    inp = Input(shape=(maxlen,))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=False)(inp)
+    x = Bidirectional(CuDNNGRU(128, return_sequences=True))(x)
+    x = Bidirectional(CuDNNGRU(100, return_sequences=True))(x)
+    x = Bidirectional(CuDNNGRU(64, return_sequences=True))(x)
+    x = Attention(maxlen)(x)
+    x = Dense(1, activation="sigmoid")(x)
+    model = Model(inputs=inp, outputs=x)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
+
+
+# 4. Train and predict
+
+## https://www.kaggle.com/strideradu/word2vec-and-gensim-go-go-go
+def train_pred(model, epochs=2):
+    for e in range(epochs):
+        model.fit(train_X, train_y, batch_size=512, epochs=1, validation_data=(val_X, val_y))
+        pred_val_y = model.predict([val_X], batch_size=1024, verbose=0)
+    pred_test_y = model.predict([test_X], batch_size=1024, verbose=0)
+    return pred_val_y, pred_test_y
+
+
+## Main part: load, train, pred and blend
+
+train_X, val_X, test_X, train_y, val_y, word_index = load_and_prec()
+vocab = []
+for w, k in word_index.items():
+    vocab.append(w)
+    if k >= max_features:
+        break
+embedding_matrix_1 = load_glove(word_index)
+# embedding_matrix_2 = load_fasttext(word_index)
+embedding_matrix_3 = load_para(word_index)
+
+embedding_matrix = np.mean([embedding_matrix_1, embedding_matrix_3], axis=0)
+np.shape(embedding_matrix)
+
+outputs = []
+
+pred_val_y, pred_test_y = train_pred(model_gru_atten_3(embedding_matrix), epochs=3)
+outputs.append([pred_val_y, pred_test_y, '3 GRU w/ atten'])
+
+pred_val_y, pred_test_y = train_pred(model_gru_srk_atten(embedding_matrix), epochs=2)
+outputs.append([pred_val_y, pred_test_y, 'gru atten srk'])
+
+pred_val_y, pred_test_y = train_pred(model_cnn(embedding_matrix_1), epochs=2)  # GloVe only
+outputs.append([pred_val_y, pred_test_y, '2d CNN GloVe'])
+
+pred_val_y, pred_test_y = train_pred(model_lstm_du(embedding_matrix), epochs=2)
+outputs.append([pred_val_y, pred_test_y, 'LSTM DU'])
+
+pred_val_y, pred_test_y = train_pred(model_lstm_atten(embedding_matrix), epochs=3)
+outputs.append([pred_val_y, pred_test_y, '2 LSTM w/ attention'])
+
+pred_val_y, pred_test_y = train_pred(model_lstm_atten(embedding_matrix_1), epochs=3)  # Only GloVe
+outputs.append([pred_val_y, pred_test_y, '2 LSTM w/ attention GloVe'])
+
+pred_val_y, pred_test_y = train_pred(model_lstm_atten(embedding_matrix_3), epochs=3)  # Only Para
+outputs.append([pred_val_y, pred_test_y, '2 LSTM w/ attention Para'])
+
+# pred_test_y = np.sum([outputs[i][1] * weights[i] for i in range(len(outputs))], axis = 0)
+coefs = [0.20076554, 0.07993707, 0.11611663, 0.14885248, 0.15734404, 0.17454667, 0.14288361]
+# pred_test_y = np.mean([outputs[i][1] for i in range(len(outputs))], axis = 0)
+pred_test_y = np.sum([outputs[i][1] * coefs[i] for i in range(len(coefs))], axis=0)
+
+pred_test_y = (pred_test_y > 0.34).astype(int)
+test = pd.read_csv("../input/test.csv", usecols=["qid"])
+sub = pd.DataFrame({"qid":test["qid"].values})
+sub['prediction'] = pred_test_y
+sub.to_csv("submission.csv", index=False)
+
